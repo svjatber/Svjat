753f5da06b7ccbb91d7d593866ddbda2cd9fec9e
svjatber <svjat.ber3003@gmail.com>
2021-03-29 02:33:28 +0300

    'done'

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <template>
   <div>
-    <h2>{{title}}</h2>
-
-    <input type="text" v-model="searchName">
-
-    <ul>
-      <li v-for="name of filteredNames">{{name}}</li>
-    </ul>
-
-    <hr>
-
-    <app-list></app-list>
+    <h2>Form inputs</h2>
+    <input type="text" v-model.lazy="name">
+    <p>{{name}}</p>
   </div>
 </template>
 
 <script>
-
-import ListMixins from './mixins'
-import List from "./List";
 export default {
   data(){
     return {
       title: 'Parent',
-
-    }
-  },
-  mixins: [ListMixins],
-  components:{
-    appList: List
-  },
-  filters:{
-    lowercase(value){
-      return value.toLowerCase()
+      name: ''
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 
-Vue.filter('uppercase', (value)=>{
-  return value.toUpperCase()
-})
-
-Vue.mixin({
-  showCreate() {
-    console.log('show000')
-  }
-})
 
 new Vue({
   el: '#app',
