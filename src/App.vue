37caab9e43487b1c315a495fffb66ac15b118d6c
svjatber <svjat.ber3003@gmail.com>
2021-03-29 15:02:32 +0300

    'done'

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,33 @@
             <input type="email"
                    id="email"
                    class="form-control"
+                   @blur="$v.email.$touch()"
                    v-model="email"
             >
           </label>
         </div>
+        <pre>{{$v.email}}</pre>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+import required from "vuelidate/lib/validators/required";
+import email from "vuelidate/lib/validators/email";
+
 export default {
   data() {
     return {
       email: ''
+    }
+  },
+  validations:{
+    email:{
+      required,
+      email
+
     }
   }
 }
