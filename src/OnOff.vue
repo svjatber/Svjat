e7196bac1a689ee12503648f26333f0f2be02abf
svjatber <svjat.ber3003@gmail.com>
2021-03-29 14:15:13 +0300

    'bootstap'

---
+++ src/OnOff.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button class="on" :class="{'active': value}" @click="onChange(true)">On</button>
+    <button class="off" :class="{'active': !value}" @click="onChange(false)">Off</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  data(){
+    return {
+      enabled: true
+
+    }
+  },
+  methods:{
+    onChange(value){
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.on.active{
+  background: green;
+}
+.off.active{
+  background: red;
+}
+</style>
